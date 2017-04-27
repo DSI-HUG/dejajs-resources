8f1eec4ed7c9576e646417d1ef8bea7b23d661ab
rtrompier <remy.trompier@gmail.com>
2017-04-27 16:34:02 +0200

    Travis conf

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,4 +1,15 @@
 VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
 
 git tag $VERSION
+
+# Get the deploy key by using Travis's stored variables to decrypt id_rsa.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../id_rsa.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 git push --tags
